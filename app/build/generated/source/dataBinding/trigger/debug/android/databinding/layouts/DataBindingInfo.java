f190550b906caa8ebb0baff43ebdeb0141142dba
teresaj <teresa.jimenezarreola@thetrainline.com>
2018-09-04 13:57:18 +0100

    initial commit

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="4c6e8182-3d8d-429a-83c6-e3966ac4dba5")
+public class DataBindingInfo {}
